8f938ec56ad0288ec21bcc1a8cf5a8556a98b171
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-08-19 09:53:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,42 +8,43 @@ function cookieClick(number){
 var cursors = 0;
 
 function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    var cursorCost = Math.floor(10 * Math.pow(1.05,cursors));     //works out the cost of this cursor
     if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
         cursors = cursors + 1;                                   //increases number of cursors
     	cookies = cookies - cursorCost;                          //removes the cookies spent
         document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    var nextCost = Math.floor(10 * Math.pow(1.05,cursors));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 var robots = 0;
 
 function buyRobot(){
-    var robotCost = Math.floor(100 * Math.pow(1.2,robots));     //works out the cost of this robot
+    var robotCost = Math.floor(20 * Math.pow(1.1,robots));     //works out the cost of this robot
     if(cookies >= robotCost){                                   //checks that the player can afford the robot
         robots = robots + 1;                                   //increases number of robots
     	cookies = cookies - robotCost;                          //removes the cookies spent
         document.getElementById('robots').innerHTML = robots;  //updates the number of robots for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(100 * Math.pow(1.2,robots));       //works out the cost of the next robot
+    var nextCost = Math.floor(20 * Math.pow(1.1,robots));       //works out the cost of the next robot
     document.getElementById('robotCost').innerHTML = nextCost;  //updates the robot cost for the user
 };
 
 var bakers = 0;
 
 function buyBaker(){
-    var bakerCost = Math.floor(1000 * Math.pow(1.2,bakers));     //works out the cost of this baker
+    var bakerCost = Math.floor(40 * Math.pow(1.1,bakers));     //works out the cost of this baker
     if(cookies >= bakerCost){                                   //checks that the player can afford the baker
         bakers = bakers + 1;                                   //increases number of bakers
     	cookies = cookies - robotCost;                          //removes the cookies spent
-        document.getElementById('bakers').innerHTML = bakers;  //updates the number of bakers for the user
+        cookiesPerSecond
+	document.getElementById('bakers').innerHTML = bakers;  //updates the number of bakers for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(1000 * Math.pow(1.2,bakers));       //works out the cost of the next baker
+    var nextCost = Math.floor(40 * Math.pow(1.1,bakers));       //works out the cost of the next baker
     document.getElementById('bakerCost').innerHTML = nextCost;  //updates the baker cost for the user
 };
 
@@ -57,10 +58,10 @@ window.setInterval(function(){
 	
 	cookieClick(robots);
 	
-}, 900)
+}, 800)
 
 window.setInterval(function(){
 	
 	cookieClick(bakers);
 	
-}, 800)
+}, 600)
